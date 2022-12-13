4d18255e2b6237e064b5b38486afe582a005e70e
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2022-12-13 13:26:34 +0100

    getters y setters creados

---
+++ zoo/Foca.java
@@ -0,0 +1,57 @@
+
+public class Foca {
+	private String nombre;
+	private String nombreCientifico;
+	private String clase;
+	private String alimentacion;
+	private double largo;
+	private double edad;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombreCientifico() {
+		return nombreCientifico;
+	}
+	
+	public void setNombreCientifico(String nombreCientifico) {
+		this.nombreCientifico = nombreCientifico;
+	}
+	
+	public String getClase() {
+		return clase;
+	}
+	
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+	
+	public String getAlimentacion() {
+		return alimentacion;
+	}
+	
+	public void setAlimentacion(String alimentacion) {
+		this.alimentacion = alimentacion;
+	}
+	
+	public double getLargo() {
+		return largo;
+	}
+	
+	public void setLargo(double largo) {
+		this.largo = largo;
+	}
+	
+	public double getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(double edad) {
+		this.edad = edad;
+	}
+}
