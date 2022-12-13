bff1dd0d3ea4eb9e84cd7d761e3c411663f6586c
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2022-12-13 13:12:28 +0100

    getters and setters

--- zoo/Pinguino.java
+++ zoo/Pinguino.java
@@ -1,4 +1,35 @@
 
 public class Pinguino {
 	
+	private String nombre;
+	private int edad;
+	private String lugarProcedencia;
+	private String color;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getLugarProcedencia() {
+		return lugarProcedencia;
+	}
+	public void setLugarProcedencia(String lugarProcedencia) {
+		this.lugarProcedencia = lugarProcedencia;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
 }
