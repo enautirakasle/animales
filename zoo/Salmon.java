69c72a30f38218dcd0401b3a9deacd063a10cb23
Julen Cano <Julen Cano@HZ321158.H012108.NET>
2022-12-13 13:31:49 +0100

    ajustado codigo

--- zoo/Salmon.java
+++ zoo/Salmon.java
@@ -1,10 +1,8 @@
-
 public class Salmon {
 	private String tipoDePescado;
 	private String aguaQmeGusta;
 	int peso;
 	double tamaño;
-
 	public void presentarme() {
 		this.tamaño = 120.5;
 		this.peso = 35;
@@ -13,21 +11,16 @@ public class Salmon {
 		System.out.println("Hola, soy un salmón\n soy carne " + tipoDePescado + "\nvivo en agua" + aguaQmeGusta
 				+ "\npeso " + peso + "kg" + "\ny mido " + tamaño + "cm\n ¡Es un placer conocerte!");
 	}
-
 	public String getTipoDePescado() {
 		return tipoDePescado;
 	}
-
 	public void setTipoDePescado(String tipoDePescado) {
 		this.tipoDePescado = tipoDePescado;
 	}
-
 	public String getAguaQmeGusta() {
 		return aguaQmeGusta;
 	}
-
 	public void setAguaQmeGusta(String aguaQmeGusta) {
 		this.aguaQmeGusta = aguaQmeGusta;
 	}
-
 }
