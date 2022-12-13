68411cb76fee6326d5bd824e573b5d0928ba07ca
Markos Martinez <Markos Martinez@HZ114826.H012108.NET>
2022-12-13 13:27:42 +0100

    Getters y Setters actualizados y meotodo presentarme credo.

---
+++ zoo/PezOscar.java
@@ -0,0 +1,49 @@
+
+public class PezOscar {
+
+	private String especie;
+	private int tamaño;
+	private double peso;
+	private String estado;
+	private int edad;
+	
+	public void presentarme() {
+		this.especie = "Pez Óscar";
+		this.tamaño = 33;
+		this.peso = 1.6;
+		this.estado = "Moribundo";
+		this.edad = 15;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public double getTamaño() {
+		return tamaño;
+	}
+	public void setTamaño(int tamaño) {
+		this.tamaño = tamaño;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}	
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
