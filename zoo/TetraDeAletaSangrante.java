ffab174a19a1c425d611205b7bbaecf7e6a5bfd2
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2022-12-13 13:23:28 +0100

    mejor

--- zoo/TetraDeAletaSangrante.java
+++ zoo/TetraDeAletaSangrante.java
@@ -1,4 +1,47 @@
-
 public class TetraDeAletaSangrante {
-
+	
+	private String nombre;
+	private double tamaño;
+	private double pesoGr;
+	private int Edadmeses;
+	private int cantAletas;
+	
+	
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getTamaño() {
+		return tamaño;
+	}
+	public void setTamaño(double tamaño) {
+		this.tamaño = tamaño;
+	}
+	public double getPesoGr() {
+		return pesoGr;
+	}
+	public void setPesoGr(double pesoGr) {
+		this.pesoGr = pesoGr;
+	}
+	public int getEdadmeses() {
+		return Edadmeses;
+	}
+	public void setEdadmeses(int edadmeses) {
+		Edadmeses = edadmeses;
+	}
+	public int getCantAletas() {
+		return cantAletas;
+	}
+	public void setCantAletas(int cantAletas) {
+		this.cantAletas = cantAletas;
+	}
+	
+	
+	
 }
