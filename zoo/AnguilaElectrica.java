1b37ed7c820433d41088c3b80d0fee5a8acfa198
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2022-12-13 13:29:13 +0100

    Anguila hecho.

---
+++ zoo/AnguilaElectrica.java
@@ -0,0 +1,79 @@
+
+public class AnguilaElectrica {
+	
+	private String tipo;
+	private String especie;
+	private Double peso;
+	private String color;
+	private int edad;
+	private String habitat;
+	private String voltaje;
+	private String estado;
+	
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getEspecia() {
+		return especie;
+	}
+	public void setEspecia(String especia) {
+		this.especie = especia;
+	}
+	public Double getPeso() {
+		return peso;
+	}
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public String getVoltaje() {
+		return voltaje;
+	}
+	public void setVoltaje(String voltaje) {
+		this.voltaje = voltaje;
+	}
+	
+	public void presentarme() {
+		this.tipo = "Animal acuatico";
+		this.edad = 2;
+		this.voltaje = "2v";
+		this.estado = "Sano";
+		this.peso = 3.2;
+		this.habitat = "Mar pacifico";
+		this.especie = "Pez";
+		this.color = "Azul";
+		
+	}
+}
