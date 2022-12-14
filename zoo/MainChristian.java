e2c8a2b636e00af40d7c859da72c4951bf8299b4
Christian Camarasa <Christian Camarasa@HZ114830.H012108.NET>
2022-12-14 09:48:35 +0100

    commit

---
+++ zoo/MainChristian.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class MainChristian {
+
+	public static void main(String[] args) {
+
+		caballitoDeMar caballo = new caballitoDeMar();
+		caballo.presentarme();
+		
+		LeonMarino leon = new LeonMarino();
+		leon.presentarse("Jose", "rojo", 2, 5, 8);
+		
+		
+		
+	}
+}
